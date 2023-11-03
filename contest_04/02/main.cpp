db27ae0c79bc79b65acd5fcdf9b9b086bf0dbfc7
MIDNIGHT <149622635+TheMidnight0@users.noreply.github.com>
2023-11-03 19:25:23 +0300

    main.cpp

---
+++ contest_04/02/main.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <fstream>
+#include "json.hpp"
+using namespace std;
+using json = nlohmann::json;
+
+int main()
+{
+    int result = 0, id;
+    cin >> id;
+    ifstream file("data.json");
+    json js = file;
+    for (json::iterator it = js.begin(); it != js.end(); ++it) {
+        for (int i = 0; i < it.value()["tasks"].size(); ++i) {
+            if (it.value()["tasks"][i]["user_id"] == id && it.value()["tasks"][i]["completed"]) {
+                result++;
+            }
+        }
+    }
+    cout << result;
+}
