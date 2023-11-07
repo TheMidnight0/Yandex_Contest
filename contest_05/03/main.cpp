b9ad0c99fca51ae593eda255a6f8e5227ae32c71
MIDNIGHT <149622635+TheMidnight0@users.noreply.github.com>
2023-11-07 22:59:14 +0300

    main.cpp

---
+++ contest_05/03/main.cpp
@@ -0,0 +1,76 @@
+#include <math.h>
+
+class Complex {
+public:
+    double re;
+    double im;
+    Complex() {}
+    Complex(std::string number) {
+        std::string temp;
+        bool sec_part = false;
+        for (int i = 0; i <= number.length(); i++) {
+            if (i == number.length()) {
+                this->im = std::stod(temp);
+                break;
+            }
+            if (number[i] == '+') {
+                if (temp != "" && temp != "-") {
+                    this->re = std::stod(temp);
+                    temp = "";
+                }
+            }
+            if (number[i] == '-') {
+                if (temp != "" && temp != "-") {
+                    this->re = std::stod(temp);
+                    temp = "-";
+                }
+                else {
+                    if (temp == "-") {
+                        temp = "";
+                    }
+                    else {
+                        temp = "-";
+                    }
+                }
+            }
+            if (number[i] != ' ' && number[i] != '+' && number[i] != '-') {
+                temp += number[i];
+            }
+        }
+    }
+};
+
+Complex operator +(Complex first, Complex second) {
+    Complex result;
+    result.re = first.re + second.re;
+    result.im = first.im + second.im;
+    return result;
+}
+
+Complex operator -(Complex first, Complex second) {
+    Complex result;
+    result.re = first.re - second.re;
+    result.im = first.im - second.im;
+    return result;
+}
+
+Complex operator *(Complex first, Complex second) {
+    Complex result;
+    result.re = first.re * second.re - first.im * second.im;
+    result.im = first.im * second.re + first.re * second.im;
+    return result;
+}
+
+Complex operator /(Complex first, Complex second) {
+    Complex result;
+    result.re = (first.re * second.re + first.im * second.im) / (pow(second.re, 2) + pow(second.im, 2));
+    result.im = (first.im * second.re - first.re * second.im) / (pow(second.re, 2) + pow(second.im, 2));
+    return result;
+}
+
+std::ostream& operator<<(std::ostream& out, const Complex& num) {
+    out << (num.re == 0.0 ? 0.0 : num.re)
+        << (num.im < 0 ? "" : "+")
+        << (num.im == 0.0 ? 0.0 : num.im) << 'j';
+    return out;
+}
