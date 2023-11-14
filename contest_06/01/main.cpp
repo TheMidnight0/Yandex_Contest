1f2250c90c95bd9c296e0470d0443363fe4abafc
MIDNIGHT <149622635+TheMidnight0@users.noreply.github.com>
2023-11-14 14:31:38 +0300

    main.cpp

--- contest_06/01/main.cpp
+++ contest_06/01/main.cpp
@@ -1,1 +1,54 @@
-.
+#include <math.h>
+
+class Shape {
+public:
+    int shape;
+    int a;
+    int b;
+    int c;
+    double getPerimeter();
+};
+
+class Circle: public Shape {
+public:
+    Circle(int r) {
+        this->shape = 1;
+        this->a = r;
+    }
+};
+
+class Rectangle : public Shape {
+public:
+    Rectangle(int a, int b) {
+        this->shape = 2;
+        this->a = a;
+        this->b = b;
+    }
+};
+
+class Triangle: public Shape {
+public:
+    Triangle(int a, int b, int c) {
+        this->shape = 3;
+        this->a = a;
+        this->b = b;
+        this->c = c;
+    }
+};
+
+double Shape::getPerimeter() {
+    double result = 0;
+    if (shape == 1) {
+        const double PI = acos(-1);
+        result = 2 * PI * a;
+    }
+    if (shape == 2) {
+        result = a + a + b + b;
+    }
+    if (shape == 3) {
+        if (a < (b + c) && b < (a + c) && c < (a + b)) {
+            result = a + b + c;
+        }
+    }
+    return result;
+}
